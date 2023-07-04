4e121e7793b07a2412761e8da9baa2b09949140c
Razvi42 <razvik42@gmail.com>
2023-07-04 15:56:16 +0300

    Implemented API request on click to display advices

--- advice-generator-app/src/App.jsx
+++ advice-generator-app/src/App.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import pattern from "./images/pattern-divider-desktop.svg";
 import dice from "./images/icon-dice.svg";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  const getAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        console.log(response.data.slip);
+        setAdvice(response.data.slip);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="flex justify-center items-center ">
-      <div className="relative bg-darkGrayBlue w-[600px] h-[400px] rounded-3xl flex flex-col items-center px-4 ">
-        <span className=" text-primaryNeon text-xl uppercase tracking-widest pt-10 max-h-20 ">Advice</span>
-        <p className="text-primaryLight text-2xl tracking-wider text-center h-40 pt-8">
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, incidunt! Voluptatem reiciendis consequatur
-          eaque rerum nobis accusantium non eligendi facilis!"
+      <div className="relative bg-darkGrayBlue w-[400px] h-[400px] lg:w-[450px] lg:h-[300px] rounded-3xl flex flex-col items-center px-6 ">
+        <span className=" text-primaryNeon text-sm uppercase tracking-widest pt-10 max-h-20 ">Advice #{advice.id}</span>
+        <p className="text-primaryLight text-xl tracking-wider text-center pt-6 max-h-48 lg:max-h-36">
+          "{advice.advice}"
         </p>
-        <img src={pattern} alt="pattern" className="pt-20" />
-        <button className="bg-primaryNeon w-10 h-10 rounded-full flex justify-center items-center absolute -bottom-5 hover:shadow-lg hover:shadow-primaryNeon transition-shadow duration-150 cursor-pointer ">
+        <img src={pattern} alt="pattern" />
+        <button
+          onClick={getAdvice}
+          className="bg-primaryNeon w-10 h-10 rounded-full flex justify-center items-center absolute -bottom-5 hover:shadow-lg hover:shadow-primaryNeon transition-shadow duration-150 cursor-pointer ">
           <img src={dice} alt="dice" className=" bg-primaryNeon w-5 h-5 " />
         </button>
       </div>
